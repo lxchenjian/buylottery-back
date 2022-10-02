802b7b8424948e76147e67da7cadb19bd4002339
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-10-02 10:00:46 +0800

    Initial commit

---
+++ src/test/java/com/lucky/buylotteryback/BuylotteryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.buylotteryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuylotteryBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
